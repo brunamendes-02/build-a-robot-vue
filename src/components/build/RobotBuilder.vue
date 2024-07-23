b71eac77897490cf8bf98193ba1f9340c7c8e0bb
brunamendes-02 <bruna.kcn@gmail.com>
2024-07-23 10:10:27 -0300

    scope style and multiples css classes

--- src/components/build/RobotBuilder.vue
+++ src/components/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part">
+      <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
           {{ selectedRobot.head.title }}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
@@ -42,13 +42,13 @@
         <thead>
           <tr>
             <th>Robot</th>
-            <th class=cost>Cost</th>
+            <th class="cost">Cost</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(robot, index) in cart" :key="index">
-            <td>{{robot.head.title}}</td>
-            <td class="cost">{{robot.cost}}</td>
+            <td>{{ robot.head.title }}</td>
+            <td class="cost">{{ robot.cost }}</td>
           </tr>
         </tbody>
       </table>
@@ -83,6 +83,9 @@ export default {
     };
   },
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : '';
+    },
     selectedRobot() {
       return {
         head: this.availableParts.heads[this.selectedHeadIndex],
@@ -167,7 +170,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .part {
   position: relative;
   width: 165px;
@@ -276,12 +279,16 @@ export default {
   padding: 3px;
   font-size: 16px;
 }
-th, td {
+th,
+td {
   text-align: left;
   padding: 5px;
   padding-right: 20px;
 }
 .cost {
   text-align: right;
+}
+.sale-border {
+  border: 3px solid red;
 }
 </style>
