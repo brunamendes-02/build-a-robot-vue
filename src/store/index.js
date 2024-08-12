c6b0fdebc1946f958c84bd0ac3ee0deb410c8241
brunamendes-02 <bruna.kcn@gmail.com>
2024-08-12 10:19:36 -0300

    use axios to get data from server

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
     cart: [],
+    parts: null,
   },
   mutations: {
     addRobotToCart(state, robot) {
       state.cart.push(robot);
+    },
+    updateParts(state, parts) {
+      state.parts = parts;
+    },
+  },
+  actions: {
+    getParts({ commit }) {
+      axios.get('/api/parts')
+        .then((result) => commit('updateParts', result.data)).catch(console.error);
     },
   },
   getters: {
